4928771db99d26d8df33a217d6c03c7b0e2f4e39
杨亮 <yangliang@114study.com>
2021-05-28 18:35:08 +0800

    编辑文章

--- main.go
+++ main.go
@@ -265,8 +265,6 @@ func main() {
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
 
-	router.HandleFunc("/articles/{id:[1-9]+}/edit", articlesEditHandler).Methods("GET").Name("articles.edit")
-	router.HandleFunc("/articles/{id:[0-9]+}", articlesUpdateHandler).Methods("POST").Name("articles.update")
 	router.HandleFunc("/articles/{id:[0-9]+}/delete", articlesDeleteHandler).Methods("GET").Name("articles.delete")
 
 	router.Use(forceHtmlMiddleware)
