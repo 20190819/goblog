4b90372fb426d58025ca3c4a5ebbb76d28c2d6d3
yangl-win10-home <1034211002@qq.com>
2021-05-31 22:17:01 +0800

    重构中间件

--- app/http/middlewares/remove_trailing_slash.go
+++ app/http/middlewares/remove_trailing_slash.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -8,6 +9,7 @@ import (
 // 除首页以外，移除所有请求路径后面的斜杠
 
 func RemoveTrailingSlash(next http.Handler) http.Handler {
+	fmt.Println("hello test")
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
 			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
